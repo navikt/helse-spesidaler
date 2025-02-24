cd585cfea503936470e4ffa7dffb21961d365518
Amalie Mansåker <amalie.erdal.mansaker@nav.no>
2025-02-24 15:02:44 +0100

    Unngår ugyldige perioder i databasen
    Co-authored-by: David Steinsland <david@davidsteinsland.net>
    Co-authored-by: Erik Maximilian Forsman <erik.maximilian.forsman@nav.no>
    Co-authored-by: Hege Haavaldsen <hege.haavaldsen@nav.no>
    Co-authored-by: Sivert Lunsæter <Sivert.Frang.Lunseter@nav.no>
    Co-authored-by: Linus Johansson <Linus.Sebastian.Johansson@nav.no
    Co-authored-by: Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
    Co-authored-by: Vemund Strøm <vemund.strom@nav.no>

--- api/src/main/resources/db/migration/V01__initial.sql
+++ api/src/main/resources/db/migration/V01__initial.sql
@@ -3,11 +3,12 @@ CREATE TABLE inntekt
     id                  BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     løpenummer          BIGSERIAL,
     opprettet           TIMESTAMPTZ NOT NULL DEFAULT now(),
-    personident TEXT NOT NULL,
+    personident         TEXT NOT NULL,
     kilde               TEXT NOT NULL,
     daglig_beløp_ører   INT NOT NULL,
     fom                 DATE NOT NULL,
-    tom                 DATE
+    tom                 DATE,
+    CONSTRAINT gyldig_periode CHECK (tom IS NULL OR tom >= fom)
 );
 
 CREATE INDEX inntekt_pid_idx ON inntekt(personident);

c6472350cdaf81f223f933a35f1b4a3f74da8bad
Amalie Mansåker <amalie.erdal.mansaker@nav.no>
2025-02-25 11:12:31 +0100

    Finner gjeldende inntekter gitt en periode
    Co-authored-by: David Steinsland <david@davidsteinsland.net>
    Co-authored-by: Erik Maximilian Forsman <erik.maximilian.forsman@nav.no>
    Co-authored-by: Sivert Lunsæter <Sivert.Frang.Lunseter@nav.no>
    Co-authored-by: Linus Johansson <Linus.Sebastian.Johansson@nav.no
    Co-authored-by: Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
    Co-authored-by: Vemund Strøm <vemund.strom@nav.no>

---
+++ api/src/main/kotlin/no/nav/helse/spesidaler/api/GjeldendeInntekter.kt
@@ -0,0 +1,37 @@
+package no.nav.helse.spesidaler.api
+
+import no.nav.helse.spesidaler.api.Beløp.Oppløsning.Daglig
+import no.nav.helse.spesidaler.api.Periode.Companion.trim
+
+internal class GjeldendeInntekter(personident: String, periode: Periode, dao: InntektDao) {
+    val inntekter = dao.hent(personident, periode)
+        .groupBy { it.kilde }
+        .mapValues { (_, inntekterUt) ->
+            inntekterUt.sortedByDescending { inntektUt ->
+                inntektUt.løpenummer
+            }
+        }
+        .mapValues { (_, inntekterUt) ->
+            inntekterUt.map { inntektUt ->
+                val overlapp = Periode(inntektUt.fom, inntektUt.tom ?: periode.endInclusive).overlappendePeriode(periode) ?: error("Denne overlapper jo ikke?")
+                val beløp = inntektUt.beløp.takeIf { it.oppløsning == Daglig } ?: error("Så langt har vi ikke kommet")
+                GjeldendeInntekt(inntektUt.kilde, overlapp, Beløp.Daglig(beløp.ører))
+            }
+        }
+        .mapValues { (_, gjeldendeInntekter) ->
+            gjeldendeInntekter.fold(emptyList<GjeldendeInntekt>()) { sammenslått, aktuell->
+                val nyePerioder = sammenslått.map { it.periode }
+                    .sortedBy { it.start }
+                    .trim(aktuell.periode)
+                sammenslått + nyePerioder.map {
+                    aktuell.copy(periode = it)
+                }
+            }
+        }.values.flatten().toSet()
+
+    data class GjeldendeInntekt(val kilde: String, val periode: Periode, val beløp: Beløp)
+    sealed interface Beløp {
+        val ører: Int
+        data class Daglig(override val ører: Int): Beløp
+    }
+}
---
+++ api/src/test/kotlin/no/nav/helse/spesidaler/api/GjeldendeInntekterTest.kt
@@ -0,0 +1,50 @@
+package no.nav.helse.spesidaler.api
+
+import no.nav.helse.spesidaler.api.Beløp.Oppløsning.Daglig
+import no.nav.helse.spesidaler.api.GjeldendeInntekter.Beløp.*
+import no.nav.helse.spesidaler.api.GjeldendeInntekter.GjeldendeInntekt
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+internal class GjeldendeInntekterTest {
+    private val orgnummer = "999999999"
+    private val personident = "11111111111"
+
+    @Test
+    fun `ingen overlappende perioder`() {
+        databaseTest {
+            val dao = InntektDao(it)
+
+            settInn(1000, 1.januar, 31.januar, dao)
+
+            val gjeldendeInntekter = GjeldendeInntekter(personident, Periode(1.januar, 31.januar), dao).inntekter
+            val forventedeInntekter = setOf(GjeldendeInntekt(orgnummer, Periode(1.januar, 31.januar), Daglig(1000)))
+
+            assertEquals(forventedeInntekter, gjeldendeInntekter)
+        }
+    }
+
+    @Test
+    fun `omsluttet ny inntekt gir gammel snute og hale, men ny mage`() = databaseTest {
+        val dao = InntektDao(it)
+
+        settInn(1000, 1.januar, 31.januar, dao)
+        settInn(2000, 10.januar, 20.januar, dao)
+
+        val gjeldendeInntekter = GjeldendeInntekter(personident, Periode(1.januar, 31.januar), dao).inntekter
+        val forventedeInntekter = setOf(
+            GjeldendeInntekt(orgnummer, Periode(1.januar, 9.januar), Daglig(1000)),
+            GjeldendeInntekt(orgnummer, Periode(10.januar, 20.januar), Daglig(2000)),
+            GjeldendeInntekt(orgnummer, Periode(21.januar, 31.januar), Daglig(1000))
+        )
+
+        assertEquals(forventedeInntekter, gjeldendeInntekter)
+    }
+
+    private fun settInn(ører: Int, fom: LocalDate, tom: LocalDate, dao: InntektDao) {
+        dao.lagre(InntektInn(personident, orgnummer, Beløp(ører, Daglig), fom, tom))
+    }
+
+    private val Int.januar get() = LocalDate.of(2018, 1, this)
+}

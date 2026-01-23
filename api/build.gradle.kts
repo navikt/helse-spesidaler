db290dcf25a9bf3c7f4662e342567e3ccf866978
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2026-01-23 12:24:28 +0100

    Bumper logback pga CVE

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,5 @@
 val tbdLibsVersion: String by project
-val logbackClassicVersion = "1.5.18"
+val logbackClassicVersion = "1.5.25"
 val logbackEncoderVersion = "8.0"
 val jacksonVersion = "2.18.3"
 val ktorVersion = "3.2.3" // bør være samme som i <com.github.navikt.tbd-libs:naisful-app>
